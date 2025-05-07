7cb405fba0d7255a995760256302ee04286f1187
Alexdosantos <alexdosantoss@yahoo.com>
2025-05-07 18:19:44 -0300

    feat: initialize project with TypeORM, PostgreSQL, Class Validator, Class Transformer, Swagger and nestj/config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+  app.setGlobalPrefix('api/v1');
+  app.enableCors();
+  await app.listen(config.get('PORT') || 3000);
 }
 bootstrap();
